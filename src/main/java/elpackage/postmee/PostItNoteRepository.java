06c4b50933fee73a8b3dfa54a3bff942f13bfeb2
Bryn <bryng21@hotmail.com>
2017-09-07 20:11:41 +0200

    deleted geox

--- src/main/java/elpackage/postmee/PostItNoteRepository.java
+++ src/main/java/elpackage/postmee/PostItNoteRepository.java
@@ -1,7 +1,5 @@
 package elpackage.postmee;
 
-import javafx.geometry.Pos;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
