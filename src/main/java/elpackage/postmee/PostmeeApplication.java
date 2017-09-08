70a30bc637d5dbc20d26d32eb3b38dde5ed93a47
Bryn <bryng21@hotmail.com>
2017-09-08 14:01:51 +0200

    k

--- src/main/java/elpackage/postmee/PostmeeApplication.java
+++ src/main/java/elpackage/postmee/PostmeeApplication.java
@@ -105,7 +105,7 @@ class MvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("web/theMainPage.html");
-		
+
 	}
 }
 
